7e67326b23bd46b1826640131fdc71d46519f196
Helder <heldrida@gmail.com>
2016-08-15 01:20:30 +0100

    lifecycle method where we will fetch the data from, test

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class PostsIndex extends Component {
+	componentWillMount() {
+		console.log("this will be a good time to call the action creator to fetch posts");
+	}
+
 	render() {
 		return (
 			<div>List of blog posts!</div>
