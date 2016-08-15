29e55dda76af86d5a6c38033fbb8ee17faeb04c4
Helder <heldrida@gmail.com>
2016-08-15 01:06:57 +0100

    created action creator

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
+import PostsIndex from './components/posts_index';
 
 import App from './components/app';
 
-const Greeting = (props) => {
-	return <div>Hey there!</div>
-};
-
 export default (
 	<Route path="/" component={App}>
-		<Route path="greet" component={Greeting} />
-		<Route path="greet2" component={Greeting} />
-		<Route path="greet3" component={Greeting} />
+		<IndexRoute component={PostsIndex} />
 	</Route>
 );
